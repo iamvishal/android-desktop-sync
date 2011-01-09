a9649a3961deee5026656e660ef556da9f6ea372
gnanaswaroop@gmail.com <gnanaswaroop@gmail.com@b77502b9-177c-7e1a-9cb6-46b25d9a19dd>
2011-01-09 19:01:45 +0000

    Modified the code to do bi-directional sync. TODO 1) Create an icon for the application 2) Remove logging to improve security locally 3) Create binaries and maintain versioning on downloads

--- trunk/DesktopServer/src/com/swaroop/projects/JavaDesktopClipboardAccessServer.java
+++ trunk/DesktopServer/src/com/swaroop/projects/JavaDesktopClipboardAccessServer.java
@@ -6,11 +6,9 @@ import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -31,17 +29,12 @@ public class JavaDesktopClipboardAccessServer {
 
 		Thread serverThread = new Thread(new ServerThread());
 		serverThread.start();
-
-		//		Thread clientThread = new Thread(new ClientThread());
-		//		clientThread.start();
-
-		//		Clipboard clipboard = readClipBoard();
-		//
-//		setDesktopClipBoard();
-		//
-		//		readClipBoard();
 	}
 
+	/**
+	 * Sets the clipboard to the server cache
+	 * @param toUpdateDesktopClipboard
+	 */
 	private static void setDesktopClipBoard(String toUpdateDesktopClipboard) {
 		// Set's a text into the clipboard. 
 		StringSelection stringSelection = new StringSelection(toUpdateDesktopClipboard);
@@ -50,32 +43,41 @@ public class JavaDesktopClipboardAccessServer {
 		cachedDesktopClipboard = toUpdateDesktopClipboard;
 	}
 
+	/**
+	 * Reads the clipboard and sets it to the cache if changed.
+	 * @return - the clipboard information that's read.
+	 * @throws UnsupportedFlavorException
+	 * @throws IOException
+	 */
 	private static String readClipBoard() throws UnsupportedFlavorException, IOException {
+
 		// Reads what's in the clipboard. 
 		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 		Transferable contents = clipboard.getContents(null);
 		String clipBoardContent = ((String) contents.getTransferData(DataFlavor.stringFlavor));
-		cachedDesktopClipboard = clipBoardContent;
-		System.out.println("Text in the clipboard is \n" + clipBoardContent);
+		
+		System.out.println("Text in the clipboard is " + clipBoardContent);
 		return clipBoardContent;
 	}
 
 	public static boolean isDesktopClipboardLatest() {
 		try {
 			String latestDesktopClipboard = readClipBoard();
-			return cachedDesktopClipboard.equals(latestDesktopClipboard);
+			boolean isDesktopClipboardAlreadyInSync = cachedDesktopClipboard.equals(latestDesktopClipboard);
+			System.out.println("Server clipboard is " + (isDesktopClipboardAlreadyInSync  ? "not" : "") + " modified" );
+			return isDesktopClipboardAlreadyInSync;
 		} catch (UnsupportedFlavorException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return false;
 	}
 
 	public static boolean isMobileClipboardLatest(String mobileClipboardLatest) {
-		return cachedMobileClipboard.equals(mobileClipboardLatest);
+		boolean isMobileClipboardAlreadyInSync = cachedMobileClipboard.equals(mobileClipboardLatest);
+		System.out.println("Mobile clipboard is " + (isMobileClipboardAlreadyInSync ? "not" : "") + " modified" );
+		return isMobileClipboardAlreadyInSync;
 	}
 
 	private static class ServerThread implements Runnable {
@@ -88,86 +90,80 @@ public class JavaDesktopClipboardAccessServer {
 				while(true) {
 					Socket client = serverSocket.accept();
 
-					System.out.println("Synching information");
-					BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-					String inputString = null;
-					StringBuffer totalReceivedString = new StringBuffer();
-					while ((inputString = in.readLine()) != null) {
-						//System.out.println(inputString);
-						System.out.println("read line " + inputString);
-						totalReceivedString.append(inputString);
-					}
+					// Check if data has been sent by the device and receive it
+					ObjectInputStream ois  = readDataFromDevice(client);
 					
-//					in.close();
-					// Mobile clipboard has been sent for updation.
-					if(!"".equals(totalReceivedString.toString())) {
-						System.out.println("Mobile sent its clipboard " + totalReceivedString);
-						setDesktopClipBoard(totalReceivedString.toString());
-					} else {
-						System.out.println("Mobile clipboard didn't change, received dummy request");
-					}
-					
-//					// Check if server clipboard has been updated, then send it back to device
-//					BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-//					if(!isDesktopClipboardLatest()) {
-//						System.out.println("Appears desktop clipboard has been updated too, sending it to device");
-//						String latestDesktopClipboard = readClipBoard();
-//						out.write(latestDesktopClipboard);
-//					} else {
-//						out.write("");
-//					}
-//					
-//					out.flush();
-//					out.close();
-					
+					// Check if the data has to be sent to the device.
+					ObjectOutputStream oos = sendDataToDevice(client);
 
+					ois.close();
+					oos.close();
 				}
 
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} 
-			
-//			catch (UnsupportedFlavorException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			} 
-			finally {
+			} catch (UnsupportedFlavorException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} finally {
 				try {
 					serverSocket.close();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 
 		}
+
+		/**
+		 * Sends the server clipboard if changed
+		 * @param client
+		 * @throws IOException
+		 * @throws UnsupportedFlavorException
+		 */
+		private ObjectOutputStream sendDataToDevice(Socket socket)
+		throws IOException, UnsupportedFlavorException {
+
+			// Check if server clipboard has been updated, then send it back to device
+			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+			
+			if(!isDesktopClipboardLatest()) {
+				System.out.println("Appears desktop clipboard has been updated, sending it to device");
+				String latestDesktopClipboard = readClipBoard();
+				oos.writeObject(latestDesktopClipboard);
+			} else {
+				oos.writeObject("");
+			}
+			
+			return oos;
+		}
+
+		/**
+		 * Reads the data that was sent by the Android device
+		 * @param socket
+		 * @throws IOException
+		 * @throws ClassNotFoundException 
+		 */
+		public static ObjectInputStream readDataFromDevice(Socket socket) throws IOException, ClassNotFoundException {
+
+			System.out.println("Synching information");
+			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+			
+			String message = (String) ois.readObject();
+						
+			// Mobile clipboard has been sent for updation.
+			if(!"".equals(message)) {
+				System.out.println("Mobile sent its clipboard " + message);
+				setDesktopClipBoard(message);
+			} else {
+				System.out.println("Mobile clipboard didn't change, received dummy request");
+			}
+			
+			return ois;
+
+		}
 	}
-
-
-
-	//	private static class ClientThread implements Runnable {
-	//
-	//		@Override
-	//		public void run() {
-	//			InetAddress serverAddress;
-	//			try {
-	//				serverAddress = InetAddress.getByName("10.177.223.231");
-	//				Socket socket = new Socket(serverAddress, 8585);
-	////				while(true) {
-	//					PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
-	//							.getOutputStream())), true);
-	//					// where you issue the commands
-	//					out.println("Client to Server, echo echo");
-	////				}
-	//			} catch (UnknownHostException e) {
-	//				e.printStackTrace();
-	//			} catch (IOException e) {
-	//				e.printStackTrace();
-	//			}
-	//
-	//		}
-
 }
 
 
